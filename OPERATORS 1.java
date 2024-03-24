1633ddb8656b1ac8766ecaef635a6308ebb65254
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-24 22:23:33 +0530

    Create OPERATORS 1.java

---
+++ OPERATORS 1.java
@@ -0,0 +1,20 @@
+public class hello {
+    public static void main(String[] args) {
+      
+      int a=20+30;
+      int b=a+50;
+      int c=20;
+      c+=20;
+      
+      
+      System.out.println(a);
+      System.out.println(b); 
+      System.out.println(c); 
+      System.out.println(a>=b); 
+      System.out.println(a>30 && a<100);   
+      // System.out.println(p);
+      // System.out.println(k);
+
+    }
+}
+  
