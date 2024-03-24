b3f0fd657696e2e1d445c13bfa260edd054e1b23
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-24 21:48:08 +0530

    Create VARIABLE 1.java

---
+++ VARIABLE 1.java
@@ -0,0 +1,17 @@
+public class hello {
+    public static void main(String[] args) {
+      System.out.println("hello");
+      System.out.println(33);
+      
+      String a="vijay";
+      int b;
+      b=25;
+      final int c=45;
+      //c=22;                 /*The final local variable c cannot be assigned. It must be blank and not using a compound assignment*/
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+
+    }
+}
+  
