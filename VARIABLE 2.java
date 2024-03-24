c991c42a1f080f44270062f3c08293ca97ef73bc
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-24 22:05:58 +0530

    Create VARIABLE 2.java

---
+++ VARIABLE 2.java
@@ -0,0 +1,23 @@
+public class hello {
+    public static void main(String[] args) {
+      
+      byte m=127;
+      short n=32767;
+      int a=1 ,b=2,c=3;
+      int x,y,z;
+      x=y=z=20;
+      long o=23466678945656L;
+      float p=22.566f;
+      double k=34.5667878d;     
+      
+      System.out.println(m);
+      System.out.println(n); 
+      System.out.println(a+b+c); 
+      System.out.println(x+y+z); 
+      System.out.println(o);   
+      System.out.println(p);
+      System.out.println(k);
+
+    }
+}
+  
