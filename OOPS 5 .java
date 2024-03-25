267744c39ff4a6e0940620e5307c0d32fe9d0e54
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 17:06:03 +0530

    Create OOPS 5 .java

---
+++ OOPS 5 .java
@@ -0,0 +1,17 @@
+public class hello {
+
+    int x;
+    int a;
+    public hello(int y){
+      x=50;
+      a=y;
+    }
+    
+    public static void main(String[] args) {
+      hello obj=new hello(20);
+      System.out.println(obj.x);
+      System.out.println(obj.a);
+
+    }   
+}
+  
