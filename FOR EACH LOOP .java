4b2aae442cc6731d42742e7418f26f6a10189966
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 09:42:30 +0530

    Create FOR EACH LOOP .java

---
+++ FOR EACH LOOP .java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String[] args) {
+      
+        String[] bikes={"BMW","ZX10R","RC","DUKE"};
+        for(String i:bikes){
+          System.out.println(i);
+        }  
+    }
+}
+  
