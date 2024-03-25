01aeabd8d92760f433dcbbddb8e62e22692018e6
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-26 01:36:44 +0530

    Create SCANNER 2 .java

---
+++ SCANNER 2 .java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class hello {
+  public static void main (String[] args){
+    Scanner scan=new Scanner(System.in);
+    System.out.println("What`s your name ?");
+    String name=scan.nextLine();
+    System.out.println("How old are you ?");
+    int age=scan.nextInt();
+    scan.nextLine();
+    System.out.println("What is your favourite food ?");
+    String food=scan.nextLine();
+
+    System.out.println("Hello "+name);
+    System.out.println("You are "+age+" years old");
+    System.out.println("You like "+food);
+  } 
+}
