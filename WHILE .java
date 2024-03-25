2d1411a78310c2f90ad815e6746df82d7aade2f0
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 09:32:04 +0530

    Create WHILE .java

---
+++ WHILE .java
@@ -0,0 +1,11 @@
+public class hello {
+    public static void main(String[] args) {
+      
+      int i=1;
+      while(10>=i){
+      System.out.println(i);
+      i++;
+      }     
+    }
+}
+  
