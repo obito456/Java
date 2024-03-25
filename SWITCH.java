0820ab9da0ad900f19668bccb754d612cdce1822
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 09:27:55 +0530

    Create SWITCH.java

---
+++ SWITCH.java
@@ -0,0 +1,24 @@
+public class hello {
+    public static void main(String[] args) {
+      
+      char x='c';
+      switch (x){
+        case 'a':
+        System.out.println("app");
+        break;
+        case 'b':
+        System.out.println("ball");
+        break;
+        case 'c':
+        System.out.println("cat");
+        break;
+        case 'd':
+        System.out.println("dog");
+        break;
+        case 'e':
+        System.out.println("eagle");
+        break;
+      }
+    }
+}
+  
