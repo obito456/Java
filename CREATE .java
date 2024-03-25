59e5d1c76fdc52941bf5f46160d1faf90291067c
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 09:45:08 +0530

    Create CREATE .java

---
+++ CREATE .java
@@ -0,0 +1,14 @@
+public class hello {
+    public static void main(String[] args) {
+      
+        for(int i=1;i<=20;i++){
+          if(i%2==0){
+            continue;
+          }
+          else{
+            System.out.println(i);
+          }
+        }  
+    }
+}
+  
