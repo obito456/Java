18af87793007d695e8e054c9798f7660393317f5
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 17:10:02 +0530

    Create OOPS 6 .java

---
+++ OOPS 6 .java
@@ -0,0 +1,17 @@
+public class hello {
+
+    int x;
+    String y;
+    public hello(int a,String b){
+      x=a;
+      y=b;
+    }
+    
+    public static void main(String[] args) {
+      hello obj=new hello(24,"vijay");
+      System.out.println(obj.x+" "+obj.y);
+      
+
+    }   
+}
+  
