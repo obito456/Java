d16fa341838351b959581a30eda37354d0f439f2
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 10:11:45 +0530

    Create FUNCTIONS 1 .java

---
+++ FUNCTIONS 1 .java
@@ -0,0 +1,12 @@
+public class hello {
+
+    static void myfunc(String a){
+      System.out.println("hi hello"+a);
+    }
+    public static void main(String[] args) {
+      myfunc(" vijay");
+      myfunc(" riya");
+      myfunc(" luffy");    
+    }   
+}
+  
