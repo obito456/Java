d3dbdb908aa4cb5065a4c6be4338ac86ba806643
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 06:48:35 +0530

    Create STRING 2.java

---
+++ STRING 2.java
@@ -0,0 +1,17 @@
+public class hello {
+    public static void main(String[] args) {
+      
+      String a="ms dhoni \n is the \"best\" catain";
+      String b="ms dhoni \r is the \"best\" batsmen";
+      String c="ms dhoni  is the \"best\" \t wicketkeeper";
+      String d="vijay\bkumar";
+      
+      
+      System.out.println(a);
+      System.out.println(b); 
+      System.out.println(c); 
+      System.out.println(d); 
+      
+    }
+}
+  
