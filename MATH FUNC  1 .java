622eec93131d6ae4e6f60543516c1803152a5a39
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-26 01:54:30 +0530

    Update and rename MATH FUNC.java to MATH FUNC  1 .java

---
+++ MATH FUNC  1 .java
@@ -0,0 +1,17 @@
+public class hello {
+  public static void main(String[] args) {
+    
+    int a=(int)(Math.random()*101);
+    
+    System.out.println(Math.min(5,6));
+    System.out.println(Math.max(5,6));
+    System.out.println(Math.sqrt(25));
+    System.out.println(Math.abs(-5.67));
+    System.out.println(Math.random());
+    System.out.println(Math.round(25.56));
+    System.out.println(Math.ceil(25.7));
+    System.out.println(Math.floor(25.7));
+    System.out.println(a);
+    
+  }
+}
