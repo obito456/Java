e51fd01810bcf12673fc718854d7966696abc875
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 06:56:10 +0530

    Create MATH FUNC.java

---
+++ MATH FUNC.java
@@ -0,0 +1,15 @@
+public class hello {
+    public static void main(String[] args) {
+      
+      int a=(int)(Math.random()*101);
+      
+      System.out.println(Math.min(5,6));
+      System.out.println(Math.max(5,6));
+      System.out.println(Math.sqrt(25));
+      System.out.println(Math.abs(-5.67));
+      System.out.println(Math.random());
+      System.out.println(a);
+      
+    }
+}
+  
