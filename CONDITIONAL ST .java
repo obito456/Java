44fba998dc71d9cc224fa49ab222d0f9d005e533
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 09:20:02 +0530

    Create CONDITIONAL ST .java

---
+++ CONDITIONAL ST .java
@@ -0,0 +1,18 @@
+public class hello {
+    public static void main(String[] args) {
+      
+      int a=25;
+
+      if(a>30){
+        System.out.println("if true");
+      }
+      else if(a==30){
+        System.out.println("else if true");
+      }
+      else{
+        System.out.println("else true");
+      }
+      
+    }
+}
+  
