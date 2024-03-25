d80e69b09b26d463d3d84693b8099a78c1d21c52
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 09:24:05 +0530

    Create SHORTHAND TERENARY.java

---
+++ SHORTHAND TERENARY.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String[] args) {
+      
+      int a=25;
+
+      String b=(a<20)? "if true" : "else true";
+      System.out.println(b);
+    }
+}
+  
