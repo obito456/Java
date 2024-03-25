4cd134ad9e74bd1a5d8766c0c383e0f9d168bfa4
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 10:21:19 +0530

    Create FUNCTIONS 3 .java

---
+++ FUNCTIONS 3 .java
@@ -0,0 +1,11 @@
+public class hello {
+
+    static int myfunc(int a,int b){
+      return a+b;
+    }
+    public static void main(String[] args) {
+      System.out.println(myfunc(20,30));  
+      System.out.println(myfunc(10,20));  
+    }   
+}
+  
