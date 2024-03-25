76d69b1b88a1f400c3607f77d0fa49fd3206e926
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-25 06:41:02 +0530

    Create STRING 1.java

---
+++ STRING 1.java
@@ -0,0 +1,20 @@
+public class hello {
+    public static void main(String[] args) {
+      
+      String a="ms dhoni is the \"best\" catain";
+      String b="HE`S IS \\ RIGHT";
+      String c="20";
+      String d="30";
+      int e=50;
+      
+      System.out.println(a.length());
+      System.out.println(a.toUpperCase()); 
+      System.out.println(b.toLowerCase()); 
+      System.out.println(a.indexOf("best")); 
+      System.out.println(a.concat(b));   
+      System.out.println(c+d);
+      System.out.println(d+e);
+
+    }
+}
+  
