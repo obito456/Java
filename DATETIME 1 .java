bbb6e414584a540ff625d3c08658841284cc727a
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-26 11:51:10 +0530

    Create DATETIME 1 .java

---
+++ DATETIME 1 .java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;  
+
+public class hello {  
+  public static void main(String[] args) {  
+    LocalDateTime myObj = LocalDateTime.now();
+    System.out.println(myObj);
+  }  
+}  
