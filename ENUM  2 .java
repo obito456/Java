61925bb6b0a5957e52619a9799c0b0894bada06c
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-26 11:45:04 +0530

    Create ENUM  2 .java

---
+++ ENUM  2 .java
@@ -0,0 +1,12 @@
+public class hello { 
+  enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+  }
+
+  public static void main(String[] args) { 
+    Level myVar = Level.MEDIUM; 
+    System.out.println(myVar); 
+  } 
+}
