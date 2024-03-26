68f85dacd38e198d36ec778af08f851f566e9b8f
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-03-26 15:41:53 +0530

    Create ARRAYLIST  1 .java

---
+++ ARRAYLIST  1 .java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class hello { 
+  public static void main(String[] args) { 
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+  } 
+}
