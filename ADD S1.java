a97e4447f6bda14fd0f109d6c215c643f66121c7
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-07 13:26:58 +0530

    Create ADD S1.java

---
+++ ADD S1.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String args[]) {
+    String firstName = "MS";
+    String lastName = "Dhoni";
+    System.out.println(firstName + " " + lastName);
+  }
+}
