62507d6bfacb20e0aafc41855309b94f1f87be82
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-07 13:27:44 +0530

    Create ADD S2.java

---
+++ ADD S2.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    String firstName = "Jeetega ";
+    String lastName = "congress";
+    System.out.println(firstName.concat(lastName));
+  }
+}
