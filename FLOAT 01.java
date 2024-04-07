d2be3f5320c5dd909d7a084e7ab77fa6d48a198f
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-07 13:02:44 +0530

    Create FLOAT 01.java

---
+++ FLOAT 01.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int myInt = 9;
+    float flt = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);
+    System.out.println(flt);
+  }
+}
