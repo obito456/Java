a57f2be18d5866aa498c685f26fd13252fe4b8c0
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-07 13:25:11 +0530

    Create UPPLOW 01.java

---
+++ UPPLOW 01.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    String txt = "Jadeja";
+    System.out.println(txt.toUpperCase());
+    System.out.println(txt.toLowerCase());
+  }
+}
