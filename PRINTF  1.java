b0928052f1d5814fb74b62ae102894d0f06f64d6
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-11 18:24:07 +0530

    Create PRINTF  1.java

---
+++ PRINTF  1.java
@@ -0,0 +1,14 @@
+public class hello {
+  public static void main(String[] args) {
+    int aa=23;
+    double bb=44;
+    char cc='$';
+    String dd="vijay";
+    Boolean ee=true;
+    System.out.printf("%d ",aa);
+    System.out.printf("%f ",bb);
+    System.out.printf("%c ",cc);
+    System.out.printf("%s ",dd);
+    System.out.printf("%b ",ee);
+  }
+}
