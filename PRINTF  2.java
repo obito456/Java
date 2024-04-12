96e6d4258a36873b42d5ec2096bcea1cb5cda3e9
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-12 10:36:04 +0530

    Create PRINTF  2.java

---
+++ PRINTF  2.java
@@ -0,0 +1,20 @@
+public class hello {
+  public static void main(String[] args) {
+
+    double bb=44;
+    String dd="vijay kumar";
+    String da="vj uchiha";
+    String db="world";
+
+    System.out.printf("%f ",bb);
+    System.out.printf("%.3f ",bb);
+    System.out.printf("%20f ",bb);
+    System.out.printf("%020f ",bb);
+    System.out.printf("%,f ",bb);
+    System.out.printf("%+f ",bb);
+    System.out.printf("%s ",dd);
+    System.out.printf("%20s ",da);
+    System.out.printf("%-20s ",db);
+
+  }
+}
