6e0a2e03f77db423a838a708c828fc85fa3419ab
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-05 19:52:23 +0530

    Create Wrapper  02.java

---
+++ Wrapper  02.java
@@ -0,0 +1,19 @@
+public class pack {
+
+    public static void main(String[] args){
+
+    Integer a=Integer.valueOf(50);
+    Integer b=Integer.valueOf(100);
+
+    int c =a.intValue();
+    int d =b.intValue();
+
+
+    System.out.println(a);
+    System.out.println(b);
+
+    System.out.println(c);
+    System.out.println(d);
+
+    }
+}
