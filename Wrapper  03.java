e3a11a130c6caf864a82e092fa63a54e31282718
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-05 19:54:03 +0530

    Create Wrapper  03.java

---
+++ Wrapper  03.java
@@ -0,0 +1,13 @@
+public class pack {
+
+    public static void main(String[] args){
+
+    String a="143";
+
+    int b =Integer.parseInt(a);
+
+    System.out.println(a);
+    System.out.println(b);
+
+    }
+}
