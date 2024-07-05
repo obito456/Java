41db2d77761888bff32697ef4198536aa6336e58
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-05 19:49:50 +0530

    Create Wrapper  01.java

---
+++ Wrapper  01.java
@@ -0,0 +1,18 @@
+public class pack {
+
+    public static void main(String[] args){
+
+    Integer a=Integer.valueOf(50);
+    Integer b=Integer.valueOf(50);
+
+    Integer c=100;
+    Integer d=100;
+
+    System.out.println(a==b);
+    System.out.println(a.equals(b));
+
+    System.out.println(c==d);
+    System.out.println(c.equals(d));
+
+    }
+}
