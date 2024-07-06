085ab092c0afbde569c22462a802ae55d02f3557
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-06 20:15:51 +0530

    Create Filedelete  01.java

---
+++ Filedelete  01.java
@@ -0,0 +1,13 @@
+import java.io.File;  
+
+public class pack { 
+  public static void main(String[] args) {
+    File myObj = new File("jam.java");
+    if (myObj.delete()) {
+      System.out.println("File name: " + myObj.getName());
+    } else {
+      System.out.println("The file does not exist.");
+    }
+  }
+}
+
