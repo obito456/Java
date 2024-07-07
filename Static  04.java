feeb97771485f6396f968cdcde64388d35ea61a8
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-07 18:53:49 +0530

    Create Static  04.java

---
+++ Static  04.java
@@ -0,0 +1,13 @@
+public class pack {
+
+  static class Animals {
+    public void legs() {
+      System.out.println("4 legs");
+    }
+  }
+  public static void main(String[] args) {
+
+    pack.Animals obj = new pack.Animals();
+    obj.legs();
+  }
+}
