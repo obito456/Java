77c5273c56be15f38ec5e993e8a8a93f1a3701e6
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-07 17:02:16 +0530

    Create Final  03.java

---
+++ Final  03.java
@@ -0,0 +1,20 @@
+final class Animal{
+  
+  public final void display(){
+    System.out.println("The dog color is black");
+  }
+}
+class Dog extends Animal{
+
+  void print(){
+    System.out.println("The dog color is black");
+  }
+}
+public class pack {
+  public static void main(String[] args) {
+
+    Dog dog=new Dog();
+    
+  } 
+}
+
