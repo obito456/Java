f00623cdbbc7b455fd7b6ad3d9ee172424d0e656
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-07 18:44:52 +0530

    Create Static  02.java

---
+++ Static  02.java
@@ -0,0 +1,15 @@
+class Money {  
+  static int money=30000000;
+
+  public static void cash(){
+      System.out.println("i have "+money+" rupees");
+  }
+}
+public class pack {
+  public static void main(String[] args) {
+    
+    Money.cash();
+  } 
+}
+
+ 
