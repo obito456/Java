5b69b7cbd4e95bd8aaba69defbfd2e2beb4ea57d
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-08 14:11:29 +0530

    Update and rename ABSTRACTION 1 .java to Abstraction  02 .java

---
+++ Abstraction  02 .java
@@ -0,0 +1,23 @@
+interface Animal{ 
+  abstract void eat();
+}
+
+class Lion implements Animal{
+  public void eat(){
+    System.out.println("lion eats deer");
+  }
+}
+class Dog implements Animal{
+  public void eat(){
+    System.out.println("Dog eats buiscits");
+  }
+}
+
+public class pack {
+  public static void main(String[] args) {
+      Lion lion=new Lion();
+      Dog dog =new Dog(); 
+      lion.eat();
+      dog.eat();
+  }
+}
