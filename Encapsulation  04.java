73e89d46adb7636aac49893b613b9feb35a42ae6
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-08 14:50:07 +0530

    Create Encapsulation  04.java

---
+++ Encapsulation  04.java
@@ -0,0 +1,37 @@
+class Police{
+  private String name;
+  private String crime;
+
+  Police(String name,String crime){
+    this.setname(name);
+    this.setcrime(crime);
+  }
+  public void setname(String name){
+    this.name=name;
+  }
+  public String getname(){
+    return name;
+  }
+  public void setcrime(String crime){
+    this.crime=crime;
+  }
+  public String getcrime(){
+    return crime;
+  }
+  public void copy(Police a){
+    this.setname(a.getname());
+    this.setcrime(a.getcrime());
+
+  }
+}
+
+public class pack {
+  public static void main(String[] args) {
+      Police accused1=new Police("Darshan","Murder");
+      Police accused2=new Police("Revanna","Rape");
+
+      accused2.copy(accused1);
+      System.out.println(accused1.getcrime());
+      System.out.println(accused2.getcrime());
+  } 
+}
