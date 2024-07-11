1865a0b34ae9f0ad20e56440755e1d14f56086c4
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-11 15:47:39 +0530

    Create 541. Reverse String 2 .java

---
+++ 541. Reverse String 2 .java
@@ -0,0 +1,23 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+      StringBuilder sb = new StringBuilder(s);
+  
+      for (int i = 0; i < sb.length(); i += 2 * k) {
+        int l = i;
+        int r = Math.min(i + k - 1, sb.length() - 1);
+        while (l < r) {
+          sb.setCharAt(l, s.charAt(r));
+          sb.setCharAt(r, s.charAt(l));
+          ++l;
+          --r;
+        }
+      }
+      return sb.toString();
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s="vijay";
+        int k=3;
+        System.out.println(solution.reverseStr(s,k));
+    }
+}
