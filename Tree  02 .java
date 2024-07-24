ab160b2c9173c4d8a3b9dd5916184dbe30ea2711
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-24 11:49:15 +0530

    Create Tree  02 .java

---
+++ Tree  02 .java
@@ -0,0 +1,33 @@
+public class Solution {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int val) {
+            data = val;
+            left = null;
+            right = null;
+        }
+    }
+    static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left=new Node(6);
+        root.right.right=new Node(7);
+
+        inorder(root);
+    }
+}
