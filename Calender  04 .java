424191099c4a9678225d425c77e662efea50f0f1
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-26 11:26:31 +0530

    Create Calender  04 .java

---
+++ Calender  04 .java
@@ -0,0 +1,9 @@
+import java.util.GregorianCalendar;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        GregorianCalendar gc = new GregorianCalendar();
+        System.out.println("Gregorian Calendar Date: " + gc.getTime());
+    }
+}
