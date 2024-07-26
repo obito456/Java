d3116b508751777225e0425c47c27333e53051e3
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-26 11:08:33 +0530

    Create Conversion  01 .java

---
+++ Conversion  01 .java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        Scanner scan=new Scanner(System.in);
+        String s="143";
+        int a=Integer.parseInt(s);
+        int b=100;
+        String st=String.valueOf(b);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
