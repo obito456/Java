f879ac200545ebe6823fe810b6702086270bab8b
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-29 16:43:58 +0530

    Create Default constructor  01 .java

---
+++ Default constructor  01 .java
@@ -0,0 +1,11 @@
+class Hero{
+    public Hero(){
+        System.out.println("zoro is hero");
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Hero hero=new Hero();
+    }
+}
