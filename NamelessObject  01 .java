f5c8332d6749643e69681a63f4dd82956c89e218
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-30 10:49:58 +0530

    Create NamelessObject  01 .java

---
+++ NamelessObject  01 .java
@@ -0,0 +1,12 @@
+class Animal {
+    void display() {
+        System.out.println("dog");
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+         
+        new Animal().display();
+    }
+}
