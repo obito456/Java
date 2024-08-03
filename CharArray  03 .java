9ff6987a655bbd7014d3f88c781b099690ef7ab3
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-03 17:37:54 +0530

    Create CharArray  03 .java

---
+++ CharArray  03 .java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static void main(String[] args) {
+        char[] ch={'v','i','j','a','y'};
+        String s=new String(ch);
+        System.out.println(s);
+
+        String st="welcome";
+        char[] c=st.toCharArray();
+        System.out.println(c);
+    }
+}
