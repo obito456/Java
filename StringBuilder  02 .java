49edc33e2e1e171056d4add6baf6b8e4496cd073
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-03 11:27:38 +0530

    Create StringBuilder  02 .java

---
+++ StringBuilder  02 .java
@@ -0,0 +1,7 @@
+public class Solution {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("hi vijay");
+        sb.setCharAt(1,sb.charAt(0));
+        System.out.println(sb);
+    }
+}
