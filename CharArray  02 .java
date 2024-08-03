166c63abae4ea26f478aa49d62adc65496e00622
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-03 17:34:03 +0530

    Create CharArray  02 .java

---
+++ CharArray  02 .java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static void main(String[] args) {
+        char[] ch=new char[5];
+        ch[0]='v';
+        ch[1]='i';
+        ch[2]='j';
+        ch[3]='a';
+        ch[4]='y';
+        System.out.println(ch);
+    }
+}
