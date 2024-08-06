b14d97ea447dd5716a0f887086e30d781a6d6f83
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-06 15:02:00 +0530

    Update Generics  05.1.java

--- Generics  05.1.java
+++ Generics  05.1.java
@@ -22,9 +22,11 @@ public class Solution {
   public static void main(String[] args) {
     Integer[] num1 = { 100, 120, 140, 160, 200 };
     Car<Integer> car1 = new Car<>(num1);
+    System.out.println(car1.calavg());
 
     Integer[] num2 = { 100, 120, 140, 160, 250 };
     Car<Integer> car2 = new Car<>(num2);
+    System.out.println(car2.calavg());
 
     System.out.println(car1.sameavg(car2));
   }
