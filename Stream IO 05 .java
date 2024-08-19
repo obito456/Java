58e666979c68dfa63bd8622eadb2475fb49c5077
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-19 16:53:01 +0530

    Update Stream IO 05 .java

--- Stream IO 05 .java
+++ Stream IO 05 .java
@@ -1,7 +1,7 @@
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Main {
+public class Solution {
     public static void main(String[] args) {
 
         try (FileReader fr = new FileReader("abs.txt")) {
