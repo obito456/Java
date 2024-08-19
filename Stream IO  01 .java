1b87ff2d7304c621e5db455bad6253ea353d286b
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-19 16:27:25 +0530

    Update and rename Stream  01 .java to Stream IO  01 .java

---
+++ Stream IO  01 .java
@@ -0,0 +1,16 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Solution {
+  public static void main(String[] args) {
+
+    try (FileInputStream fis = new FileInputStream("abs.txt")) {
+      int data;
+      while ((data = fis.read()) != -1) {
+        System.out.print((char) data);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
