d940fbd378e086bd4342c3ddf8f59df1a098b29f
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-19 17:08:46 +0530

    Update and rename Stream  08 .java to Stream IO 08 .java

--- Stream IO 08 .java
+++ Stream IO 08 .java
@@ -2,7 +2,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class Main {
+public class Solution {
     public static void main(String[] args) {
 
         try (BufferedWriter bw = new BufferedWriter(new FileWriter("abs.txt"))) {
