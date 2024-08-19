213a85f525bd9a3ddfe8e293a1d19516d96aebde
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-19 17:07:21 +0530

    Update and rename Stream  07 .java to Stream IO 07 .java

--- Stream IO 07 .java
+++ Stream IO 07 .java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Main {
+public class Solution {
     public static void main(String[] args) {
 
         try (BufferedReader br = new BufferedReader(new FileReader("abs.txt"))) {
