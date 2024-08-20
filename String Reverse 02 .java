58260575ceae701cb1346c569ee0a4f343959b85
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-20 11:04:51 +0530

    Create String Reverse 02 .java

---
+++ String Reverse 02 .java
@@ -0,0 +1,11 @@
+
+public class Solution {
+    public static void main(String[] args) {
+
+        String s = "vijaykumar";
+        StringBuilder sb=new StringBuilder(s);
+        sb.reverse();
+        s=sb.toString();
+        System.out.println(s);
+    }
+}
