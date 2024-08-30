f021da326909bf5661c387979a6b85efb660fca2
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-30 14:28:23 +0530

    Update Tree  04 .java

--- Tree  04 .java
+++ Tree  04 .java
@@ -1,57 +1,54 @@
+class Node {
+  int data;
+  Node left;
+  Node right;
+
+  public Node(int data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  public static int search(int[] inorder, int start, int end, int curr) {
+    for (int i = start; i <= end; i++) {
+      if (inorder[i] == curr) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  public static Node build(int[] preorder, int[] inorder, int start, int end, int[] index) {
+    if (start > end) {
+      return null;
+    }
+    int curr = preorder[index[0]];
+    index[0]++;
+    Node node = new Node(curr);
+    if (start == end) {
+      return node;
+    }
+    int pos = search(inorder, start, end, curr);
+    node.left = build(preorder, inorder, start, pos - 1, index);
+    node.right = build(preorder, inorder, pos + 1, end, index);
+
+    return node;
+  }
+  public static void inorderdisplay(Node root) {
+    if (root == null) {
+      return;
+    }
+    inorderdisplay(root.left);
+    System.out.print(root.data + " ");
+    inorderdisplay(root.right);
+  }
+}
+
 public class Solution {
-    static class Node {
-        int data;
-        Node left;
-        Node right;
-
-        Node(int val) {
-            data = val;
-            left = null;
-            right = null;
-        }
-    }
-
-    static int search(int inorder[], int start, int end, int curr) {
-        for (int i = start; i <= end; i++) {
-            if (inorder[i] == curr) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    static Node buildTree(int preorder[], int inorder[], int start, int end, int[] idx) {
-        if (start > end) {
-            return null;
-        }
-        int curr = preorder[idx[0]];
-        idx[0]++;
-        Node node = new Node(curr);
-        if (start == end) {
-            return node;
-        }
-        int pos = search(inorder, start, end, curr);
-        node.left = buildTree(preorder, inorder, start, pos - 1, idx);
-        node.right = buildTree(preorder, inorder, pos + 1, end, idx);
-
-        return node;
-    }
-
-    static void inorderPrint(Node root) {
-        if (root == null) {
-            return;
-        }
-        inorderPrint(root.left);
-        System.out.print(root.data + " ");
-        inorderPrint(root.right);
-    }
-
-    public static void main(String[] args) {
-        int preorder[] = {1, 2, 4, 3, 5};
-        int inorder[] = {4, 2, 1, 5, 3};
-
-        int[] idx = {0};  
-        Node root = buildTree(preorder, inorder, 0, inorder.length - 1, idx);
-        inorderPrint(root);
-    }
+  public static void main(String[] args) {
+    int[] preorder = { 1, 2, 4, 5, 3, 6, 7 };
+    int[] inorder = { 4, 2, 5, 1, 6, 3, 7 };
+    int[] index = { 0 };
+    Node root = Node.build(preorder, inorder, 0, preorder.length - 1, index);
+    Node.inorderdisplay(root);
+  }
 }
