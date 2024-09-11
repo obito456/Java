2749366f135bd15979d18ce83d0a5939860c5ffb
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-09-11 14:29:38 +0530

    Create Tree 16 .java

---
+++ Tree 16 .java
@@ -0,0 +1,53 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+  int data;
+  Node left;
+  Node right;
+
+  public Node(int data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  public static void rightview(Node root) {
+    if (root == null) {
+      return;
+    }
+    Queue<Node> q = new LinkedList<>();
+    q.add(root);
+
+    while (!q.isEmpty()) {
+      int n = q.size();
+      for (int i = 0; i < n; i++) {
+        Node curr = q.poll();
+
+        if (i == n - 1) {
+          System.out.println(curr.data);
+        }
+        if (curr.left != null) {
+          q.add(curr.left);
+        }
+        if (curr.right != null) {
+          q.add(curr.right);
+        }
+      }
+    }
+  }
+}
+
+public class Solution {
+  public static void main(String[] args) {
+    Node root = new Node(1);
+    root.left = new Node(2);
+    root.right = new Node(3);
+    root.left.left = new Node(4);
+    root.left.right = new Node(5);
+    root.right.left = new Node(6);
+    root.right.right = new Node(7);
+
+    Node.rightview(root);
+  }
+}
