232579b20a7a0f066b97abb0295d9749d49a5379
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-19 16:23:37 +0530

    Update PriorityQueue  01 .java

--- PriorityQueue  01 .java
+++ PriorityQueue  01 .java
@@ -2,15 +2,16 @@ import java.util.PriorityQueue;
 
 public class Solution {
     public static void main(String[] args) {
-        PriorityQueue pq=new PriorityQueue<>();
 
-        pq.add(6);
-        pq.add(5);
-        pq.add(4);
-        pq.add(8);
-        
-        System.out.println(pq);
-        System.out.println(pq.poll());
-        System.out.println(pq);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        minHeap.add(10);
+        minHeap.add(20);
+        minHeap.add(15);
+
+        System.out.println("elements: " +minHeap); 
+        System.out.println("Polling element: " + minHeap.poll()); 
+        System.out.println("Polling element: " + minHeap.poll()); 
+        System.out.println("Polling element: " + minHeap.poll());
     }
 }
