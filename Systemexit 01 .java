2b1a4fb9d6da1045e37745e44d253aa12026063c
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-11-05 11:03:38 +0530

    Create Systemexit 01 .java

---
+++ Systemexit 01 .java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hi");
+            System.exit(0);
+        } finally {
+            System.out.println("Hello");
+        }        
+    }
+}
