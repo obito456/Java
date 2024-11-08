e9acc48da662cb3fd433627a14a2c25f29592c8b
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-11-08 11:50:23 +0530

    Create Comparator 02 .java

---
+++ Comparator 02 .java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+class Student{
+    public int id;
+    public String name;
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public String toString() {
+        return "(id=" + id + ", name='" + name + ") ";
+    }
+}
+
+public class Solution{
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Bob"));
+        students.add(new Student(3, "Alice"));
+        students.add(new Student(2, "Charlie"));
+
+        System.out.println("Before sorting: " + students);
+        Comparator<Student> idComparator = Comparator.comparing(s->s.id);
+        students.sort(idComparator);
+        System.out.println("After sorting: " + students);
+    }
+}
