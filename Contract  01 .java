7ca9f978545fbc4996f1fdba9e60b03bffd585a8
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-11-10 12:22:57 +0530

    Create Contract  01 .java

---
+++ Contract  01 .java
@@ -0,0 +1,25 @@
+import java.util.Objects;
+
+class Car{
+    String color;
+    public Car(String color){
+        this.color=color;
+    }
+    public int hashCode() {
+        return 7;
+    }
+}
+
+public class Solution{
+    public static void main(String[] args) {
+        Car nano=new Car("yellow");
+        Car audi=new Car("white");
+        Car benz=new Car("white");
+
+        System.out.println(nano.equals(audi)); 
+        System.out.println(audi.equals(benz)); 
+        System.out.println(nano.hashCode()); 
+        System.out.println(audi.hashCode()); 
+        System.out.println(nano.hashCode()==audi.hashCode()); 
+    }
+}
