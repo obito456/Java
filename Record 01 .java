e49e86925b5b64387adf57c52bd4a9c9ca79da7d
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-11-17 18:53:33 +0530

    Create Record 01 .java

---
+++ Record 01 .java
@@ -0,0 +1,7 @@
+record Car(String name,String color){}
+public class Solution {
+    public static void main(String[] args) {
+       Car car=new Car("Ferrari", "Red");
+       System.out.println(car);
+    }
+}
